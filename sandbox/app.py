5ae1f4b1f52d8cdf5465b4aa270623e2dd9536c8
Mark Gekelman <gekelmam@oregonstate.edu>
2022-06-16 18:50:20 -0700

    Adding output for landing page.

--- sandbox/app.py
+++ sandbox/app.py
@@ -23,9 +23,14 @@ conn.commit()
 
 @app.route('/')
 def index():
-  return 'Add Something Here...'
+  output = dict([("/reset", "Reset all scores to zero."), 
+           ("/game", "Plays the game, and displays winner."),
+           ("/user", "Displays both of the player's scores."),
+           ("/user/1", " Displays player one's score."),
+           ("/user/2", " Displays player two's score.")])
+  return jsonify(output)
 
-#adding variables
+# Displays one player's score
 @app.route('/user/<username>')
 def show_user(username):
   c.execute("SELECT * FROM players WHERE (player_name = (?))", (username))
@@ -35,6 +40,7 @@ def show_user(username):
   response = {"Player": response[0], "Score": response[1]}
   return jsonify(response)
 
+# Displays both player's scores
 @app.route('/user')
 def user():
   c.execute("SELECT * FROM players")
@@ -44,6 +50,7 @@ def user():
     output.append({"Player": x[0], "Score": x[1]})
   return jsonify(output)
 
+# Reset all the player's scores.
 @app.route('/reset')
 def reset():
   c.execute("UPDATE players SET num_wins = 0")
@@ -51,6 +58,7 @@ def reset():
   response = {"Messege": "Successfully reset scores."}
   return jsonify(response)  
 
+# Plays the game and displays the winner.
 @app.route('/game')
 def game():
   winner = war.play()
